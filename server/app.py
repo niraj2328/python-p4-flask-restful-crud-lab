405151b10cde2c7ca989b1c1e605fbed5af20384
Niraj <niraj.patel@flatironschool.com>
2023-02-27 07:02:08 -0500

    completed

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, jsonify, request, make_response
+from flask import Flask, jsonify, request
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
 
@@ -16,37 +16,42 @@ db.init_app(app)
 
 api = Api(app)
 
-class Plants(Resource):
 
+class Plants(Resource):
     def get(self):
         plants = [plant.to_dict() for plant in Plant.query.all()]
-        return make_response(jsonify(plants), 200)
+        return jsonify(plants), 200
 
     def post(self):
-
         data = request.get_json()
-
-        new_plant = Plant(
-            name=data['name'],
-            image=data['image'],
-            price=data['price'],
-        )
-
+        new_plant = Plant(**data)
         db.session.add(new_plant)
         db.session.commit()
+        return new_plant.to_dict(), 201
 
-        return make_response(new_plant.to_dict(), 201)
-
-api.add_resource(Plants, '/plants')
 
 class PlantByID(Resource):
-
     def get(self, id):
-        plant = Plant.query.filter_by(id=id).first().to_dict()
-        return make_response(jsonify(plant), 200)
+        plant = Plant.query.filter_by(id=id).first_or_404()
+        return plant.to_dict(), 200
 
+    def patch(self, id):
+        plant = Plant.query.filter_by(id=id).first_or_404()
+        data = request.get_json()
+        for attr, value in data.items():
+            setattr(plant, attr, value)
+        db.session.commit()
+        return plant.to_dict(), 200
+
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first_or_404()
+        db.session.delete(plant)
+        db.session.commit()
+        return '', 204
+
+
+api.add_resource(Plants, '/plants')
 api.add_resource(PlantByID, '/plants/<int:id>')
-        
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
